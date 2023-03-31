645a024d9ff3d6849cab665fc155ec4a1cb89798
DonHulieo <96005229+DonHulieo@users.noreply.github.com>
2023-03-31 14:44:02 +1100

    Add files via upload

--- fivem_lua_ep1/server/main.lua
+++ fivem_lua_ep1/server/main.lua
@@ -1,1 +1,8 @@
+print('Hello, I\'m the Server') --> I'm a small chunk
 
+-- [[ EVENTS ]] --
+
+RegisterNetEvent('serverChainedEvent', function() -- I'm a Networked Event, I can be triggered from both the client and server!
+  print('I was triggered from the client!')
+  TriggerClientEvent('clientChainedEvent', -1)
+end) --> From RegisterNetEvent to end) is my chunk, when the script first starts my event name get Registered so it can be called!
