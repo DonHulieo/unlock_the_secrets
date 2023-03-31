23a53c71c4fe551eeb3dc419f0c9ce3316b3eb3f
DonHulieo <96005229+DonHulieo@users.noreply.github.com>
2023-03-31 14:42:13 +1100

    Add files via upload

---
+++ fivem_lua_ep1/fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'DonHulieo'
+description 'A Set of Examples for Function Calls in FiveM Lua'
+
+shared_scripts {'config.lua'}
+
+client_scripts {'client/main.lua'}
+
+server_scripts {'server/main.lua'}
+
+lua54 'yes'
